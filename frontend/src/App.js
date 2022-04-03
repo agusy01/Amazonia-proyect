c3238d466ef5493c1343efd79cac88f46cc6b353
strilchuk_agus <agus.strilchuk@gmail.com>
2022-04-03 18:32:57 -0300

    Rating y productos dinamicos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import data from './data';
 import './App.css';
 
+// Components
+import Product from './components/Product';
+
 
 function App() {
   return (
@@ -19,25 +22,7 @@ function App() {
         <div>
           <div className="row center">
             {data.products.map(product => (
-                <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-                    {/* image size: 680px by 830px */}
-                    <img className="medium" src={product.image} alt="product" />
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
+                <Product key={product._id} product={product} />
               ))
             }
           </div>

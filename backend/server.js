9f3eb09c360ebb71e4eb17d79a820f6dac74225f
strilchuk_agus <agus.strilchuk@gmail.com>
2022-05-30 20:12:33 -0300

    Creacion de API para place order

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import process from 'process';
 import productRouter from './routers/productRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose.connect( process.env.MONGODB_URL || 'mongodb://localhost/amazonia', {
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 
 app.get('/', (req, res) => {
     res.send('Server is ready');

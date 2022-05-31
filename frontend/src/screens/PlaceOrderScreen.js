fe236265ce6f49f3306f4d290039082f75ab878a
strilchuk_agus <agus.strilchuk@gmail.com>
2022-05-31 20:28:55 -0300

    Implementacion de la accion del boton Place Order

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { ORDER_CREATE_RESET } from '../constants/orderConstans';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const PlaceOrderScreen = () => {
     const navigate = useNavigate();
@@ -9,14 +13,24 @@ const PlaceOrderScreen = () => {
     if(!cart.paymentMethod) {
         navigate('/payment');
     }
+    const orderCreate = useSelector(state => state.orderCreate);
+    const {loading, success, error, order} = orderCreate;
     const toPrice = (num) => Number(num.toFixed(2)); 
     cart.itemsPrice = toPrice(cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0));
     cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
     cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
     cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+    const dispatch = useDispatch();
     const placeOrderHandler = () => {
-        
+        dispatch(createOrder({...cart, orderItems: cart.cartItems}))
     };
+    useEffect(() => {
+        if(success) {
+            navigate(`/order/${order._id}`);
+            dispatch({type: ORDER_CREATE_RESET});
+        }
+    }, [success, dispatch, navigate, order]);
+    
     return (
         <div>
             <CheckoutSteps step1 step2 step3 step4 />
@@ -99,6 +113,8 @@ const PlaceOrderScreen = () => {
                             <li>
                                 <button type='button' onClick={placeOrderHandler} className='primary block' disabled={cart.cartItems.length === 0}> Place Order </button>
                             </li>
+                            { loading && <LoadingBox /> }
+                            { error && <MessageBox variant="danger">{error}</MessageBox> }
                         </ul>
                     </div>
                 </div>

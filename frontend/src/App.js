12fbc25b51b794ac3045b4c071e9f44cc2154b73
strilchuk_agus <agus.strilchuk@gmail.com>
2022-06-03 19:30:49 -0300

    Historial de compra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 
 
 function App() {
@@ -52,7 +53,12 @@ function App() {
                   {userInfo.name} <i className='fa fa-caret-down'></i>{' '}
                 </Link>
                 <ul className='dropdown-content'>
-                  <Link to='#signout' onClick={signoutHandler}>Sign Out</Link>
+                  <li>
+                    <Link to='/orderhistory'>Order History</Link>
+                  </li>
+                  <li>
+                    <Link to='#signout' onClick={signoutHandler}>Sign Out</Link>
+                  </li>
                 </ul>
               </div>
             ) : (
@@ -74,6 +80,7 @@ function App() {
           <Route path='/payment' element={<PaymentMethodScreen />}></Route>
           <Route path='/placeorder' element={<PlaceOrderScreen />}></Route>
           <Route path='/order/:id' element={<OrderScreen />}></Route>
+          <Route path='/orderhistory' element={<OrderHistoryScreen />}></Route>
         </Routes>
       </main>
       <footer className="row center">All right reserved</footer>
